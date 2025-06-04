b1b9d582a6e3580b45fb178da382fb9599971240
Felipe Oliveira Rocha <felipeoliveira02114@gmail.com>
2025-06-04 17:10:39 -0300

    add files

--- typescript/28-arrayDestructuring/index.js
+++ typescript/28-arrayDestructuring/index.js
@@ -1,5 +1,13 @@
+"use strict";
 // desestruturação permite descompactar arrays ou objetos em um monte de variáveis
-//quando fazemos a desestruturação, o array original
+//quando fazemos a desestruturação, o array original continua intacto
 var person = ['John', 'Smith', 28];
 var fname = person[0], lname = person[1], age = person[2];
 console.log(fname, lname, age);
+var nome = person[0], idade = person[2];
+console.log(nome, idade);
+var primeroNome = person[0], segundoNome = person[1];
+console.log(primeroNome, segundoNome);
+// agora caso algum valor seja opcional podemos definir um valor padrão para não receber undefined
+var pnome = person[0], snome = person[1], anos = person[2], _a = person[3], gender = _a === void 0 ? 'male' : _a;
+console.log(pnome, snome, anos, gender);
--- typescript/28-arrayDestructuring/index.ts
+++ typescript/28-arrayDestructuring/index.ts
@@ -6,4 +6,14 @@ const person = ['John', 'Smith', 28];
 const [fname, lname, age] = person;
 console.log(fname, lname, age);
 
+const [nome, , idade] = person;
+console.log(nome, idade);
+
+const [primeroNome, segundoNome] = person;
+console.log(primeroNome, segundoNome);
+
+// agora caso algum valor seja opcional podemos definir um valor padrão para não receber undefined
+const [pnome, snome, anos, gender = 'male'] = person;
+console.log(pnome, snome, anos, gender)
+
 export {}; // serve para que o compilador não reclame de redeclaração de uma mesma váriavel em vários arquivos
