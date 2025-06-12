3fc593e524cdfc248b16e640b8a8776772667129
Felipe Oliveira Rocha <felipeoliveira02114@gmail.com>
2025-06-12 13:48:46 -0300

    add gitignore

--- javascript/53-function&this/index.js
+++ javascript/53-function&this/index.js
@@ -1,3 +1,27 @@
 // usar o .this em esopo global irá referenciar o objeto global que é o wwindows
-
+// no 'use strict' é undefined o this na função
 console.log(this);
+
+function foo() {
+    console.log(this);
+}
+foo();
+
+const greet = function() {
+    console.log(this);
+}
+greet();
+
+let person = {
+    name: 'jhon',
+    ageYear: 1999,
+    calculateAge: function() {
+        console.log(this);
+        function inner() {
+            console.log(this);
+        }
+        inner();
+        return 2025 - this.ageYear;
+    }
+}
+person.calculateAge();
